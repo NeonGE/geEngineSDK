2cd9d579ee9c1e06b43cc1d142d5534154df1aa2
Samuel Alejandro Prince Quezada <samuel.prince.quezada@gmail.com>
2026-01-23 19:11:39 -0600

    Refactor usage macros for platform/compiler compatibility
    Refactored gePlatformUsing.h to define usage macros based on compiler.
    Original macros are now only used for MSVC (not Clang); other platforms use new boolean logic macros for portability and consistency.

--- sdk/geUtilities/include/gePlatformUsing.h
+++ sdk/geUtilities/include/gePlatformUsing.h
@@ -1,8 +1,23 @@
 #pragma once
 
-#if !defined(USING)
-  #define IN_USE &&
-  #define NOT_IN_USE &&!
-  #define USE_IF(x) &&((x)?1:0)&&
-  #define USING(x) (1 x 1)
-#endif // #if !defined(USING)
+//We check this since this is the deepest file in the engine
+
+#if defined(_MSC_VER) && !defined(__clang__)
+# if !defined(USING)
+#   define IN_USE &&
+#   define NOT_IN_USE &&!
+#   define USE_IF(x) &&((x)?1:0)&&
+#   define USING(x) (1 x 1)
+# endif // #if !defined(USING)
+#else
+# ifndef GE_USING_BOOL
+#   define GE_USING_BOOL(x) ((x) ? 1 : 0)
+# endif
+# if !defined(USING)
+#   define USING(x)      (GE_USING_BOOL(x) == 1)
+#   define NOT_USING(x)  (GE_USING_BOOL(x) == 0)
+#   define USE_IF(x) GE_USING_BOOL(x)
+#   define IN_USE 1
+#   define NOT_IN_USE 0
+# endif // #if !defined(USING)
+#endif
