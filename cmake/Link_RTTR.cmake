fe927d22f799cdf71055490f494ce3cd58be9c0b
Samuel Alejandro Prince Quezada <samuel.prince.quezada@gmail.com>
2026-02-02 11:07:15 -0600

    Improve RTTR integration in CMake and add example usage
    - Added ge_rttr_resolve_target and ge_copy_rttr_runtime to robustly link and deploy RTTR DLLs on Windows.
    - Updated CMakeLists.txt to link RTTR and copy runtime for geUtilities and geUtilities_Tests.
    - Replaced empty RTTR registration in core_dependencies.cpp with a concrete example, including struct, enum, metadata, and runtime reflection demo.

--- cmake/Link_RTTR.cmake
+++ cmake/Link_RTTR.cmake
@@ -12,3 +12,50 @@ function(ge_link_rttr target_name)
     )
   endif()
 endfunction()
+
+function(ge_rttr_resolve_target out_var)
+  set(_t "")
+  if(TARGET RTTR::Core)
+    # RTTR::Core puede ser ALIAS
+    get_target_property(_aliased RTTR::Core ALIASED_TARGET)
+    if(_aliased)
+      set(_t "${_aliased}")
+    else()
+      set(_t "RTTR::Core")
+    endif()
+  elseif(TARGET rttr_core_lib)
+    set(_t "rttr_core_lib")
+  elseif(TARGET rttr_core)
+    set(_t "rttr_core")
+  endif()
+
+  if(_t STREQUAL "")
+    message(FATAL_ERROR "RTTR target not found (RTTR::Core/rttr_core_lib/rttr_core).")
+  endif()
+
+  set(${out_var} "${_t}" PARENT_SCOPE)
+endfunction()
+
+function(ge_link_rttr target_name)
+  ge_rttr_resolve_target(_rttr_target)
+  target_link_libraries(${target_name} PRIVATE ${_rttr_target})
+endfunction()
+
+function(ge_copy_rttr_runtime target_name)
+  if(NOT WIN32)
+    return()
+  endif()
+
+  ge_rttr_resolve_target(_rttr_target)
+
+  # Solo copia si RTTR es una DLL
+  get_target_property(_type "${_rttr_target}" TYPE)
+  if(_type STREQUAL "SHARED_LIBRARY")
+    add_custom_command(TARGET ${target_name} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different
+              "$<TARGET_FILE:${_rttr_target}>"
+              "$<TARGET_FILE_DIR:${target_name}>"
+      COMMENT "Copying RTTR DLL next to ${target_name}"
+    )
+  endif()
+endfunction()
--- tests/geUtilities_Tests/src/core_dependencies.cpp
+++ tests/geUtilities_Tests/src/core_dependencies.cpp
@@ -1,6 +1,43 @@
 #include <rttr/registration>
+#include <iostream>
+
+using namespace rttr;
+
+struct MyStruct
+{
+  MyStruct()
+  {};
+  
+  void func(double)
+  {};
+  
+  int data;
+};
+
+enum class MetaData_Type
+{
+  SCRIPTABLE,
+  GUI
+};
 
 RTTR_REGISTRATION
 {
-  //Empty registration block to force linking of core dependencies
+  registration::class_<MyStruct>("MyStruct")
+    .constructor<>()
+    .property("data", &MyStruct::data)
+      (
+        metadata(MetaData_Type::SCRIPTABLE, false),
+        metadata("Description", "This is the data")
+      )
+    .method("func", &MyStruct::func);
+}
+
+void foo()
+{
+  type t = type::get<MyStruct>();
+  for (auto& prop : t.get_properties())
+    std::cout << "name: " << prop.get_name() << std::endl;
+
+  for (auto& meth : t.get_methods())
+    std::cout << "name: " << meth.get_name() << std::endl;
 }
