8cb2b266b30e6efc1ea0a300bff3074000ce6f6d
Samuel Alejandro Prince Quezada <samuel.prince.quezada@gmail.com>
2026-02-12 10:14:09 -0600

    Fixed builds for windows - All required DLLs are copied to the output of execution

--- cmake/Externals.cmake
+++ cmake/Externals.cmake
@@ -12,3 +12,15 @@ include(${CMAKE_SOURCE_DIR}/cmake/Externals_MiniZip.cmake)
 function(ge_setup_test_dependencies)
   ge_setup_nlohmann_json()
 endfunction()
+
+function(ge_copy_runtime_dlls targetName)
+  if(WIN32 AND CMAKE_VERSION VERSION_GREATER_EQUAL "3.21")
+    add_custom_command(TARGET ${targetName} POST_BUILD
+      COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        $<TARGET_RUNTIME_DLLS:${targetName}>
+        $<TARGET_FILE_DIR:${targetName}>
+      COMMAND_EXPAND_LISTS
+      VERBATIM
+    )
+  endif()
+endfunction()
